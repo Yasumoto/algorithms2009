75bf63e72dcf4400428a84b6f9a0a281998bf812
Joe Smith <yasumoto7@gmail.com>
2009-02-11 19:19:08 -0800

    Initial Commit, getting ready for assignment 1. Signed-off-by: Joe Smith <yasumoto7@gmail.com>

---
+++ templates/m.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#double LateXing for bib consistency
+pdflatex $1.tex
+pdflatex $1.tex
+
+bibtex $1
+
+pdflatex $1.tex
+pdflatex $1.tex 
+
+
+#remove temporary files
+rm $1.bbl
+rm $1.aux
+rm $1.log
+rm $1.blg
+
